4fbe912fcdf5cdaae6da599fb6457a3a65580900
Jose Alvarez <jose.alvarez@hgs.hiddengeniusproject.org>
2021-12-06 18:49:27 -0800

    created a file in IDLE

---
+++ lesson_5.py
@@ -0,0 +1,14 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(pathToDataBase):
+    connection = None
+    try:
+        connection = sqlite3.connect(pathToDataBase)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+connection = create_connection("toDatabase")
